5ccc3b807b2b77cad77fc4f32eeac53cfb98d95f
Eben Adewumi <111870838+EbieA@users.noreply.github.com>
2023-11-07 09:17:35 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ function insert(num){
     document.form.textview.value = document.form.textview.value+num
 }
 
+
 function equal (){
     document.form.textview.value = eval(document.form.textview.value)
 }
